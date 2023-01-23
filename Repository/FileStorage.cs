90c0393b2b6a22bd685ddc206d3fed6da1cf5542
DeepakGowda1999 <m1084126@mindtree.com>
2023-01-23 09:58:12 +0530

    first commit

--- Repository/FileStorage.cs
+++ Repository/FileStorage.cs
@@ -5,7 +5,7 @@ namespace StorageAccount.Repository
 {
     public class FileStorage
     {
-        public static string connectionString="DefaultEndpointsProtocol=https;AccountName=deepakaccountstorage12;AccountKey=Ij2+egkV9s9pZLftRXGgxkE2D66gNFqeQdMm9FbZjJ/wz0Gm+476FpIq9/W6hN1IsP+19Qi3fBGF+AStM7tyCw==;EndpointSuffix=core.windows.net";
+        public static string connectionString="";
         static ShareServiceClient shareServiceClient=null;
         public static async Task CreateFile(string fileName)
         {
